4ce0876723e5baefd8485cb73cad9377714f4c65
Aleksandr Bychkov <aleksandr.bychkov@tktk.ee>
2023-11-30 15:24:10 +0200

    commit

---
+++ Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.WriteLine("HelloWorld");
+
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye();
+}
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, aligator!");
+}
